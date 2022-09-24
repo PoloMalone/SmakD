05225fff4240784e63044f7eba5b9ee7274ecff0
Josef <strutsu@hotmail.com>
2022-09-24 22:58:24 +0200

    Add AdminViewcontroller

---
+++ Smak Demokrat/ViewControllers/AdminHomeViewController.swift
@@ -0,0 +1,25 @@
+//
+//  AdminHomeViewController.swift
+//  Smak Demokrat
+//
+//  Created by Josef Jakobsson on 2022-09-24.
+//
+
+import Foundation
+import UIKit
+
+class AdminHomeViewController: UIViewController {
+
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+
+    }
+
+
+    
+
+}
+
